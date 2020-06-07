420ed231a2d71cee2d768c0c02442d65f2826091
Mateus Santos <mateusdsmarcelino@gmail.com>
2020-06-06 22:12:13 -0300

    Instalando Nunjucks

--- src/server.js
+++ src/server.js
@@ -4,20 +4,30 @@ const server = express()
 //configurar pasta publica
 server.use(express.static("public"))
 
+
+//utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("/src/views", {
+    express: server,
+    noCache: true
+
+})
+
+
 //confirgurar caminhos da minha aplicação
 //pagina inicial
 //req : Requisição
 //res: Resposta
 server.get("/", (req, res) => {
-res.sendFile(__dirname + "/views/index.html")
+res.render("index.html")
 })
 
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+   return res.render("create-point.html")
     })
 
     server.get("/search-results", (req, res) => {
-        res.sendFile(__dirname + "/views/search-results.html")
+     return res.sendFile(__dirname + "/views/search-results.html")
         })
 
 
